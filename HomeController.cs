6f1a54be6fa5519e7f68736fc1d1e557f1504662
Alparslan YÜCE <44805477+alparslanyuce@users.noreply.github.com>
2021-06-04 16:34:46 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,72 @@
+﻿using mvc_intermediate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_intermediate.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //veritabanına bağlanır ve bilgileri getirir.
+            //getirilen bilgiler bir model içerisine yani bir sınıf
+            //içerisine aktarılır ve bu model view'e gönderilir.
+
+            UrunKategoriModel model = new UrunKategoriModel();
+
+            model.UrunSayisi = Veritabanı.Liste.Where(i => i.Satistami == true).Count();
+            model.Urunler = Veritabanı.Liste.Where(i=> i.Satistami==true).ToList();
+            return View(model);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+          var urun =  Veritabanı.Liste.Where(i => i.UrunId == id).FirstOrDefault();
+
+
+
+
+            return View(urun);
+        }
+
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UrunEkle(/*string UrunAdi,string Aciklama,double Fiyat,string Resim,bool Satistami*/  Urun entity )
+        {
+            //Urun entity = new Urun();
+            //entity.UrunAdi = UrunAdi;
+            //entity.Aciklama = Aciklama;
+            //entity.Fiyat = Fiyat;
+            //entity.Resim = Resim;
+            //entity.Satistami = Satistami;
+
+            Veritabanı.ElemanEkle(entity);
+
+            return View("UrunListe", Veritabanı.Liste);
+
+            //bilgileri karşılayıp gerekli yere kayıt edelim.
+           
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Veritabanı.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_intermediate.Models
+{
+    //Sanal Veritabanı
+    //Uygulama arkasında fiziksel bir dosyada bilgiler saklanmıyor.
+    //Bilgiler bellekte saklanır.
+    //Uygulama çalıştığından sonlanana kadar bu bilgilere ulaşabiliriz.
+    public static class Veritabanı
+    {
+        private static List<Urun> _liste;
+
+        static Veritabanı()
+        {
+            _liste = new List<Urun>()
+            {
+                new Urun(){ UrunId=1, UrunAdi="Samsung S6",Aciklama="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",Fiyat=3000,Satistami=false,Resim="1.jpg"},
+                new Urun(){ UrunId=2, UrunAdi="Samsung S7", Aciklama = "idare eder", Fiyat = 5000, Satistami = true,Resim="2.jpg" },
+                new Urun(){ UrunId=3, UrunAdi="Samsung S8",Aciklama="iyi",Fiyat=7000,Satistami=true,Resim="3.jpg"},
+                new Urun(){ UrunId=4, UrunAdi="IPhone7",Aciklama="iyi",Fiyat=4800,Satistami=true,Resim="4.jpg"},
+                new Urun(){ UrunId=5, UrunAdi="IPhone11",Aciklama="çok iyi",Fiyat=8000,Satistami=true,Resim="5.jpg"}
+            };
+        }
+
+        //Veritabanı.Liste
+        public static List<Urun> Liste
+        {
+            get { return _liste; }
+        }
+
+        public static void ElemanEkle(Urun entity)
+        {
+            _liste.Add(entity);
+        }
+
+        //Urun entity = Veritabanı.UrunDetay(5)
+        public static Urun UrunDetay(int urunid)
+        {
+            Urun entity = null;
+            foreach (var urun in _liste)
+            {
+                if(urun.UrunId==urunid)
+                {
+                    entity = urun;
+                }
+            }
+
+            return entity;
+
+        }
+
+
+       
+    }
+}
